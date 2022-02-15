34357282fb6d30791b5897426ec3d9d2a61d9ac0
이윤주 <toyaann@naver.com>
2022-02-15 10:07:13 +0900

    버튼이 눌리면 > 로그 찍어보기

--- app/src/main/java/com/nepplus/kotlinbagic/MainActivity.kt
+++ app/src/main/java/com/nepplus/kotlinbagic/MainActivity.kt
@@ -2,11 +2,24 @@ package com.nepplus.kotlinbagic
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+        btnLog.setOnClickListener {
+            Log.d("메인화면","로그 버튼 눌림")
+        }
+
+
+
+
+
 
 
 
