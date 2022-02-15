d034ae0f26e40d450768ce1da649e0c1e7062771
이윤주 <toyaann@naver.com>
2022-02-15 10:15:49 +0900

    비교용 버튼 추가, 주석 작성

--- app/src/main/java/com/nepplus/kotlinbagic/MainActivity.kt
+++ app/src/main/java/com/nepplus/kotlinbagic/MainActivity.kt
@@ -12,11 +12,16 @@ class MainActivity : AppCompatActivity() {
 
 
 
+//         btnLog가 눌리면 어떤 코드를 실행할지, 할 일을 설정
         btnLog.setOnClickListener {
+
+//         로그를 찍어보는 기능 실행. => btnLog가 클릴될 때 마다 실행.
             Log.d("메인화면","로그 버튼 눌림")
         }
 
 
+
+        btnToast
 
 
 
